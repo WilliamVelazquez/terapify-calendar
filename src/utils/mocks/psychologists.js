dd7bc1cbba81116125465533ba96282ebea06eb7
William Velazquez <william.velazquez.a@gmail.com>
2020-06-22 15:07:21 -0500

    (Utilities): Creating Utilities, Creating Psychologists Mock and Creating useForm Custom Hook

---
+++ src/utils/mocks/psychologists.js
@@ -0,0 +1,24 @@
+const users = [
+  {
+    id: 1,
+    name: 'Luz Victoria Meza Morales',
+    email: 'luz@undefined.sh',
+  },
+  {
+    id: 2,
+    name: 'Aline Delgado Cárdenas',
+    email: 'aline@undefined.sh',
+  },
+  {
+    id: 3,
+    name: 'Juana Alamillo Avalos',
+    email: 'juana@undefined.sh',
+  },
+  {
+    id: 4,
+    name: 'Mauricio Alejandro Mondragón Martínez',
+    email: 'mauricio@undefined.sh',
+  },
+];
+
+export default users;
---
+++ src/utils/utilities.js
@@ -0,0 +1,36 @@
+export const getCurrentDate = () => {
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const yyyy = today.getFullYear();
+  return `${yyyy}/${mm}/${dd}`;
+};
+
+export const getCurrentTime = () => {
+  const today = new Date();
+  const hours = String(today.getHours()).padStart(2, '0');
+  const minutes = String(today.getMinutes()).padStart(2, '0');
+  const seconds = String(today.getSeconds()).padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
+};
+
+export const getlastDateAfterDays = (days = 7) => {
+  const laterDate = new Date();
+  laterDate.setDate(laterDate.getDate() + days);
+  laterDate.setHours(23,59,59,999);
+  return laterDate
+}
+
+export const formatDate = (date = new Date()) => {
+  const givenDate = new Date(date)
+  let month = '' + (givenDate.getMonth() + 1);
+  let day = '' + givenDate.getDate();
+  const year = givenDate.getFullYear();
+
+  if (month.length < 2) 
+    month = '0' + month;
+  if (day.length < 2) 
+    day = '0' + day;
+
+  return [year, month, day].join('-');
+}
