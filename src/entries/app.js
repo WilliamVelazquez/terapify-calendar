0d5840ca2676650844e69023d16d9cce950a2c95
William Velazquez <william.velazquez.a@gmail.com>
2020-06-23 15:34:41 -0500

    Deploy to gh-pages

--- src/entries/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import 'babel-polyfill';
-
-import App from '../pages/App';
-
-const container = document.getElementById('app');
-
-render(<App />, container);
